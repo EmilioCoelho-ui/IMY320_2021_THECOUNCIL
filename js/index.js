6f5b7eeeb0965d4ddbff9914d5c0bf694c363df3
ODBabalola <u18041494@tuks.co.za>
2021-10-19 18:07:46 +0200

    Separation of concerns in css and js files

---
+++ js/index.js
@@ -0,0 +1,11 @@
+AOS.init({
+    duration: 1200,
+})
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
